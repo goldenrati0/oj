74ef3e45e08d0551e2076246520ea02be4a5169f
TameInteractive <tameinteractive@gmail.com>
2016-11-28 07:23:38 +0600

    1001 1003

---
+++ uri-oj/1003/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+         
+         Scanner ana = new Scanner(System.in);
+         
+         int a = ana.nextInt();
+         int b = ana.nextInt();
+         
+         int sum = a+b;
+         
+         System.out.println("SOMA = " + sum);
+    
+    }
+ 
+}
