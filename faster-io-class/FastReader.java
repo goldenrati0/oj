f1ae94d603e3575d6f6b8d380c85a336d56ba51e
tahmid-choyon <tahmid.choyon@gmail.com>
2017-02-24 01:36:32 +0600

    Faster IO class modified

--- faster-io-class/FastReader.java
+++ faster-io-class/FastReader.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 static class FastReader
