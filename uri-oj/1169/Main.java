68317c111c405d6cf475e20ef211ba7cb3b70271
tahmid-choyon <tahmid.choyon@gmail.com>
2017-02-24 01:37:30 +0600

    uri 1169

---
+++ uri-oj/1169/Main.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+/**
+ * Created by tahmid on 2/24/17.
+ */
+public class Main {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastReader ana = new FastReader();
+        BigInteger toKG = new BigInteger("12000");
+        int test = ana.nextInt();
+        for (int tc = 0; tc < test; tc++) {
+
+            BigInteger grains = new BigInteger("2");
+            grains = grains.pow(ana.nextInt());
+            grains = grains.divide(toKG);
+            System.out.println(grains.toString() + " kg");
+        }
+    }
+}
