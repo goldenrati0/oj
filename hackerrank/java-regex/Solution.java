1425520c7c3a3a2cc3f63d68c6becb24d87e7fbd
tahmid-choyon <tahmid.choyon@gmail.com>
2017-03-27 04:54:35 +0600

    Hackerrank Java Regex

---
+++ hackerrank/java-regex/Solution.java
@@ -0,0 +1,23 @@
+package java_regex;
+
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+
+    public static String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+}
