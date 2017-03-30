1d9afb0a5fc37e81b007b1cc319161fa26f2d1d2
tahmid-choyon <tahmid.choyon@gmail.com>
2017-03-31 04:13:36 +0600

    Hackerrank Staircase

---
+++ hackerrank/staircase/Solution.java
@@ -0,0 +1,75 @@
+package staircase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Created by tahmid on 3/30/17.
+ */
+public class Solution {
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        float nextFloat() {
+            return Float.parseFloat(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastReader ana = new FastReader();
+
+        int n = ana.nextInt();
+
+        for(int i=1; i<=n; i++) {
+
+            for(int j=1; j<=n-i; j++)
+                System.out.print(" ");
+            for(int j=1; j<=i; j++)
+                System.out.print("#");
+            System.out.println();
+        }
+    }
+}
